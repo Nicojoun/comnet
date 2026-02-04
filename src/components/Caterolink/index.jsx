e52c452f4c2f266f8ed5d5a158113d0b74d373db
Nico Joun <nicojoun793@gmail.com>
2026-02-04 13:33:50 +0100

    ajoustement Carolink

--- src/components/Caterolink/index.jsx
+++ src/components/Caterolink/index.jsx
@@ -1,9 +1,22 @@
 import '../../assets/styles/Caterolink.scss';
+import { Link } from 'react-router-dom';
 
 function Caterolink({ text }) {
+  const getRoute = (text) => {
+    switch(text) {
+      case 'F.A.Q.': return '/faq';
+      case 'INFO': return '/about';
+      case 'METEO': return '/meteo';
+      case 'SERVICES': return '/services';
+      default: return '/';
+    }
+  };
+
   return (
     <div className='caterolink'>
-      <span className='caterolink-text'>{text}</span>
+      <Link to={getRoute(text)}>
+        <span className='caterolink-text'>{text}</span>
+      </Link>
     </div>
   );
 }
