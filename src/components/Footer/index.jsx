13fa9fd8f19f8e2f23e3089d3c79953de222a378
Nico Joun <nicojoun793@gmail.com>
2026-02-04 11:54:43 +0100

    Modifier footer blanc avec barre noire repositionnée et espacement Home

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,9 @@
 import '../../assets/styles/Footer.scss';
-import logo_footer from '../../assets/logo/logo_footer.png';    
-import { Link } from 'react-router-dom';  
 
 function Footer() {
   return (
     <div className='footer'>
-
+      <div className='footer-bar'></div>
     </div>
   );
 }
