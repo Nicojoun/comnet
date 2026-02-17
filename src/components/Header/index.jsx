c43bc107d37bddebeffa0d28f0407b2dcd17e264
Nico Joun <nicojoun793@gmail.com>
2026-02-17 11:39:47 +0100

    Adjust categories grid and header width spacing

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,30 +1,23 @@
-import '../../assets/styles/Header.scss'; 
-import logo from '../../assets/logo/logo.png';  
-import { Link, useLocation } from 'react-router-dom';  
+import '../../assets/styles/Header.scss';
+import { Link, useLocation } from 'react-router-dom';
 import Connect from '../Connect';
 
 function Header() {
   const location = useLocation();
-  const isQuestionPage = location.pathname === '/question'; // R�cup�ration de l'URL actuelle
+  const isQuestionPage = location.pathname === '/question';
 
   return (
     <div className={`header${isQuestionPage ? ' header--inverse' : ''}`}>
-
       <div className='header-left'>
         <span className='header-chevron'>{'<'}</span>
       </div>
 
       <nav className='header-nav'>
-
-
-        {/* Si on est sur la page d'accueil, afficher du texte soulign�, sinon un lien */}
         {location.pathname === '/' ? (
-          <span className='header-linkActive'>ESATYPIC</span>  // Texte soulign�
+          <span className='header-linkActive'>ESATYPIC</span>
         ) : (
-          <Link to='/' className='header-link'>ESATYPIC</Link>  // Lien normal
+          <Link to='/' className='header-link'>ESATYPIC</Link>
         )}
-
-
       </nav>
 
       <div className='header-right'>
